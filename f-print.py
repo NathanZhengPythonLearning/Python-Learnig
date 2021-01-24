d606d667b62e9e16902a681f16232ed1e773145b
ChoiceABC <nz09098488@gmail.com>
2021-01-24 00:11:38 -0500

    1

--- f-print.py
+++ f-print.py
@@ -1,6 +1,12 @@
+#%%
 num=123
 print(f'{num=:#x} {num=:#b} {num=:#o}')
+print(f'{num=:#X} {num=:#%} {num=:#E}')
+print(f'{num=:#E} {num=:#F} {num=:#G}')
+print(f'{num=:#n} {num=:#f} {num=:#d}')
+print(f'{num=!s:#s} {num=:#f} {num=:#d}')
 
+# The '#' option is only valid for integers, and only for binary, octal, or hexadecimal output. If present, it specifies that the output will be prefixed by '0b', '0o', or '0x', respectively.
 
 #%%
 # https://docs.python.org/2/library/string.html#format-specification-mini-language
@@ -11,6 +17,20 @@ print(f'{num=:#x} {num=:#b} {num=:#o}')
 # width       ::=  integer
 # precision   ::=  integer
 # type        ::=  "b" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G" | "n" | "o" | "s" | "x" | "X" | "%"
+# The '#' option is only valid for integers, and only for binary, octal, or hexadecimal output. If present, it specifies that the output will be prefixed by '0b', '0o', or '0x', respectively.
+# When no explicit alignment is given, preceding the width field by a zero ('0') character enables sign-aware zero-padding for numeric types. This is equivalent to a fill character of '0' with an alignment type of '='.
+
+# https://docs.python.org/3/library/string.html
+# replacement_field ::=  "{" [field_name] ["!" conversion] [":" format_spec] "}"
+# field_name        ::=  arg_name ("." attribute_name | "[" element_index "]")*
+# arg_name          ::=  [identifier | digit+]
+# attribute_name    ::=  identifier
+# element_index     ::=  digit+ | index_string
+# index_string      ::=  <any source character except "]"> +
+# conversion        ::=  "r" | "s" | "a"
+# format_spec       ::=  <described in the next section>
+
+# !r (repr), !s (str) and !a (ascii)
 
 #%%
 a=-1234567890.0947 # width=1+10+1+4, precision=4
